0127d206984f4933b5522007ab464133b614dc64
Erick Etiene <333.erick@gmail.com>
2024-01-08 09:18:37 -0300

    🧑‍💻 config: .env, tsconfig, dependences

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app";
+import { env } from "./env";
 
 const port = 3333;
 app
   .listen({
     host: "0.0.0.0",
-    port: port,
+    port: env.PORT,
   })
   .then(() => console.log(`🚀 Server Runing in ${port}`));
