162525d8ce1b3ed1b936749e08f64f7db06cd677
Lamenkazu <333.erick@gmail.com>
2024-03-02 22:57:51 -0300

    ✨ Gyms Controllers: Search Gyms, Nearby Gyms and Create New Gym Route

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { makeFetchNearbyGymsService } from "@/services/factories/make-fetch-nearby-gyms-service";
+
+export async function nearby(req: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    lat: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    long: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { lat, long } = nearbyGymsQuerySchema.parse(req.body);
+
+  const fetchNearbyGymsService = makeFetchNearbyGymsService();
+
+  const { gyms } = await fetchNearbyGymsService.execute({
+    userLat: lat,
+    userLong: long,
+  });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { makeSearchGymsService } from "@/services/factories/make-search-gyms-service";
+
+export async function search(req: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { query, page } = searchGymQuerySchema.parse(req.body);
+
+  const searchGymService = makeSearchGymsService();
+
+  const { gyms } = await searchGymService.execute({ query, page });
+
+  return reply.status(200).send({
+    gyms,
+  });
+}
